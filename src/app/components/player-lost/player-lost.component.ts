e45c257af79ce2ddbfab75f2909f5798877d282b
HHessoquat <jeremy.dequeant@3wa.io>
2024-11-03 02:37:02 +0100

    feat player lost component

--- src/app/components/player-lost/player-lost.component.ts
+++ src/app/components/player-lost/player-lost.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Player} from "../../entities/player.entity";
+import {Subscription} from "rxjs";
+import {PlayersService} from "../../services/players.service";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-player-lost',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './player-lost.component.html',
   styleUrl: './player-lost.component.scss'
 })
-export class PlayerLostComponent {
+export class PlayerLostComponent implements OnInit{
+   @Input() playerId!: number;
+   player!: Player;
+
+
+  constructor(private playerManager: PlayersService) {
+  }
+  ngOnInit() {
+    this.player = this.playerManager.getAllPlayers().find(player => player.id === this.playerId)!;
+  }
 
 }
